585f7d05a9882f588865721c22796924e85dd907
Sumanta Jyoti <sumantajyoti795@gmail.com>
2026-04-02 21:09:30 +0530

    data validation stage done & successfull runs

--- src/MLproject/utiles/common.py
+++ src/MLproject/utiles/common.py
@@ -1,11 +1,11 @@
 import os
+from box import ConfigBox
 from box.exceptions import BoxValueError
 import yaml
 from MLproject import logger
 import json
 import joblib
 from ensure import ensure_annotations
-from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
@@ -15,7 +15,7 @@ from typing import Any
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """reads yaml file and returns
+    """reads yaml file and returns a ConfigBox object
 
     Args:
         path_to_yaml (str): path like input
